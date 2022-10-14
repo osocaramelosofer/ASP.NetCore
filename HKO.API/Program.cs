7ce9526bd6674ad2e9913e08630d64ab00da29e0
ZJ\F4533561 <A95F4533561@saint-gobain.com>
2022-10-14 15:02:42 -0500

    Add Serilog config

--- HKO.API/Program.cs
+++ HKO.API/Program.cs
@@ -1,14 +1,34 @@
+using Microsoft.EntityFrameworkCore;
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// Connection with the database.
+var connString = builder.Configuration.GetConnectionString("DsapoDatabaseConnection");
+// Dbcontext represents the conexion with the database
+//builder.Services.AddDbContext<HKODbContext>(options => options.UseSqlServer(connString));
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
+// Serilog
+builder.Host.UseSerilog((ctx, lc) =>
+    lc.WriteTo.Console().ReadFrom.Configuration(ctx.Configuration));
 
+// Cors
+builder.Services.AddCors(options => {
+    options.AddPolicy("AllowAll",
+        b => b.AllowAnyMethod()
+        .AllowAnyHeader()
+        .AllowAnyOrigin());
+});
+
+var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -17,6 +37,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+// Cors
+app.UseCors("AllowAll");
 
 app.UseAuthorization();
 
